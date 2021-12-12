4e8892742437a4b305dfc08fe3105e234f6946e9
abduhakim <78894135+drizzy514@users.noreply.github.com>
2021-12-12 23:07:47 +0500

    Update posts.jsx

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -19,7 +19,7 @@ const Posts = ({ posts, loading }) => {
       {posts.map(element => (
         <li key={element.post_id} className='list-group-item'>
           <br></br>
-                                        <img src={`http://blog-siteuz.herokuapp.com/public/${element.post_img}` } height="100" with="100"/>
+                                        <img src={`/public/${element.post_img}` } height="100" with="100"/>
                                         <h3 className="video-photographer">{element.post_title}</h3>
                                         <p>{element.post_text}</p>
                                         <button onSubmit={deletePosst}>Delete post</button>
